8ce9eee636938fe49f3abc5826cd22559d73e75c
ipsum <trueman.0320@zju.edu.cn>
2024-07-11 16:22:05 +0800

    add: 两数之和_01

---
+++ Java/两数之和_1.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}

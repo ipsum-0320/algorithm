4d042faf1a3f28234278b8a4c97f94858fabe904
ipsum <trueman.0320@zju.edu.cn>
2024-07-28 20:57:35 +0800

    add

--- Java/两数之和_1.java
+++ Java/两数之和_1.java
@@ -5,12 +5,16 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> hashtable = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
-            if (hashtable.containsKey(target - nums[i])) {
-                return new int[]{hashtable.get(target - nums[i]), i};
+        // 1. Map 是一个泛型，记着添加泛型相关的。
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                // map 判断是否包含 key 使用 containsKey。
+                return new int[]{i, (int) map.get(target - nums[i])};
+            } else {
+                map.put(nums[i], i);
+                // map 添加 k v，使用 put。
             }
-            hashtable.put(nums[i], i);
         }
         return new int[0];
     }

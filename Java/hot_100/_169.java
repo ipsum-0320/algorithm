7324a893faa95cf874d2588dfef9a9744b8024e7
ipsum-0320 <trueman.0320@zju.edu.cn>
2024-09-12 14:20:07 +0800

    add

---
+++ Java/hot_100/_169.java
@@ -0,0 +1,17 @@
+package Java.hot_100;
+
+public class _169 {
+
+    public int majorityElement(int[] nums) {
+        int cand_num = nums[0], count = 1;
+        for (int i = 1; i < nums.length; ++i) {
+            if (cand_num == nums[i])
+                ++count;
+            else if (--count == 0) {
+                cand_num = nums[i];
+                count = 1;
+            }
+        }
+        return cand_num;
+    }
+}

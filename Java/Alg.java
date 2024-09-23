cc86fc7f4e6fb384560dc87b069031752184d9c8
ipsum-0320 <trueman.0320@zju.edu.cn>
2024-09-23 16:02:59 +0800

    add

---
+++ Java/Alg.java
@@ -0,0 +1,20 @@
+package Java;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Alg {
+
+}

bafa8f9ba4eff3c97b00718cd32773d7d52e3598
ipsum-0320 <trueman.0320@zju.edu.cn>
2024-10-15 10:03:08 +0800

    add

---
+++ Java/hot_100/hard/_84.java
@@ -0,0 +1,23 @@
+package Java.hot_100.hard;
+
+public class _84 {
+
+    public int largestRectangleArea(int[] heights) {
+        // 双指针。
+        int[] leftMin = new int[heights.length];
+        // 前缀最小值的下标。
+
+        int[] rightMin = new int[heights.length];
+        // 后缀最小值的下标。
+
+
+
+
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        // 单调栈解法。
+
+    }
+
+}

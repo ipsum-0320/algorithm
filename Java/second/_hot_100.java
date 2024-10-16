caf6fb8365f7f4a1c23151bece41ca9381e7e425
wangxinghao <wangxinghao@xiaohongshu.com>
2024-10-16 14:36:51 +0800

    add

---
+++ Java/second/_hot_100.java
@@ -0,0 +1,49 @@
+package Java.second;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class _hot_100 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // 通过计数的方法解决。
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+                // 字符 - 字符 = 数字，这样就可以得到字符的位置。
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (count[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    // 字符 + 数字转 chat。
+                    sb.append(count[i]);
+                }
+            }
+            String key = sb.toString();
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return map.values().stream().collect(Collectors.toList());
+        // stream API 的使用。
+    }
+}

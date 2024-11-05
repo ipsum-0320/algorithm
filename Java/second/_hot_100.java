1e8f85e506652a3c915d28d731bdf86992baeaf1
ipsum-0320 <trueman.0320@zju.edu.cn>
2024-11-05 10:45:36 +0800

    add

--- Java/second/_hot_100.java
+++ Java/second/_hot_100.java
@@ -376,4 +376,58 @@ public class _hot_100 {
         return n + 1;
     }
 
+    public void setZeroes(int[][] matrix) {
+        // 直接使用 O(1) 的时间复杂度
+        int isColZero = 1;
+        int isRowZero = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                isColZero = 0;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                isRowZero = 0;
+                break;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (isColZero == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (isRowZero == 0) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+    }
+
 }

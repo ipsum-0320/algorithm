9cd5be1634a767601f5fe8a6951b9c596cb6c3d5
wangxinghao <wangxinghao@xiaohongshu.com>
2025-03-05 20:25:40 +0800

    feat

--- Java/summer/_61_70.java
+++ Java/summer/_61_70.java
@@ -2,6 +2,7 @@ package Java.summer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class _61_70 {
     // 131-155
@@ -204,11 +205,109 @@ public class _61_70 {
 
     // 153
     public int findMin(int[] nums) {
-
-
+        if (nums[0] < nums[nums.length - 1]) return nums[0];
+        // 第一个小于末尾数字的位置。
+        int last = nums[nums.length - 1];
+        int left = 0, right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= last) right = mid;
+            else if (nums[mid] > last) left = mid;
+        }
+        return Math.min(nums[left], nums[right]);
     }
 
 
+    // 4 寻找两个有序数组的中位数
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // 首先分奇数偶数去讨论。
+        int all = nums1.length + nums2.length;
+        if (all % 2 == 0) {
+            // 如果是偶数。
+            int left = helper(nums1, nums2, 0, 0, all / 2);
+            int right = helper(nums1, nums2, 0, 0, all / 2 + 1);
+            return (left + right) / 2.0;
+        }
+        // 如果是奇数。
+        return helper(nums1, nums2, 0, 0,all / 2 + 1);
+    }
+    public int helper(int[] nums1, int[] nums2, int p1, int p2, int k) {
+        // 该函数的作用是在 nums1 和 nums2 中找**第 k 大的数字**。
+        // 为了减少讨论，保证 nums1 的 length 永远小于 nums2 的 length。
+        if (nums1.length - p1 > nums2.length - p2) {
+            // 注意需要减去 p1、p2。
+            return helper(nums2, nums1, p2, p1, k);
+        }
+
+        if (p1 >= nums1.length) {
+            return nums2[p2 + k - 1];
+        }
+
+        if (k == 1) {
+            // 找第一大的数字，直接找两者最小就可以了。
+            return Math.max(nums1[p1], nums2[p2]);
+        }
+
+        int index1 = Math.min(nums1.length, p1 + k / 2) - 1;
+        // 找到 nums1 的第 k/2 个元素。
+        // 这里不需要管多一个还是少一个，反正取出一半就行。
+        int index2 = p2 + k / 2 - 1;
+        // 找到 nums2 的第 k/2 个元素（因为是“第”，所以要 - 1）。
+
+        if (nums1[index1] > nums2[index2]) {
+            // 把较小的一段丢掉，因此把 index2 对应的丢弃（减掉 k 时需要保证抛弃的数量准确，不能因为精度问题有影响）。
+            return helper(nums1, nums2, p1, index2 + 1, k - (index2 - p2 + 1));
+        }
+        return helper(nums1, nums2, index1 + 1, p2, k - (index1 - p1 + 1));
+    }
+
+    // 20-有效的括号
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[' || s.charAt(i) == '{' || s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if (s.charAt(i) == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') return false;
+                }
+                if (s.charAt(i) == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') return false;
+                }
+                if (s.charAt(i) == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 
 
+    // 155-最小栈。
+    class MinStack {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> minStack = new Stack<>();
+
+        public MinStack() {
+        }
+
+        public void push(int val) {
+            this.stack.push(val);
+            if (this.minStack.isEmpty() ||
+                    this.minStack.peek() >= val) this.minStack.push(val);
+        }
+
+        public void pop() {
+            if (this.stack.peek().equals(this.minStack.peek())) this.minStack.pop();
+            this.stack.pop();
+        }
+
+        public int top() {
+            return this.stack.peek();
+        }
+
+        public int getMin() {
+            return this.minStack.peek();
+        }
+    }
 }

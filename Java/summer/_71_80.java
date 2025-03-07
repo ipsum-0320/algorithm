b02c262fb9fbe5a047bb0637aa35cce89778141b
wangxinghao <wangxinghao@xiaohongshu.com>
2025-03-07 13:57:24 +0800

    feat

--- Java/summer/_71_80.java
+++ Java/summer/_71_80.java
@@ -196,14 +196,14 @@ public class _71_80 {
             } else {
                 if (this.count % 2 == 0) {
                     // 想办法将 maxHeap 多一个。
-                    if (num <= maxHeap.peek()) maxHeap.add(num);
+                    if (num <= minHeap.peek()) maxHeap.add(num);
                     else {
                         maxHeap.add(minHeap.poll());
                         minHeap.add(num);
                     }
                 } else {
                     // 如果是奇数，想办法让 minHeap 多一个。
-                    if (num > minHeap.peek()) minHeap.add(num);
+                    if (num >= maxHeap.peek()) minHeap.add(num);
                     else {
                         minHeap.add(maxHeap.poll());
                         maxHeap.add(num);
@@ -216,8 +216,108 @@ public class _71_80 {
         public double findMedian() {
             // 定义如果是奇数，中位数放在最大堆，如果是偶数，就二者相加 / 2。
             if (this.count % 2 == 1) return this.maxHeap.peek();
-            return Math.round((this.maxHeap.peek() + this.minHeap.peek()) / 2.0);
+            return (this.maxHeap.peek() + this.minHeap.peek()) / 2.0;
         }
     }
+
+    // 121
+    public int maxProfit(int[] prices) {
+        int res = 0, min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            res = Math.max(res, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return res;
+    }
+
+    // 55
+    public boolean canJump(int[] nums) {
+        // 直接用 dp 来做，时间复杂度是 O(N^2)。
+        boolean[] dp = new boolean[nums.length];
+        // dp[i] 表示能否到达下标 i。
+        dp[0] = true;
+        for (int i = 1; i < nums.length; i++) {
+            int j = i - 1;
+            while (j >= 0) {
+                int step = nums[j];
+                dp[i] = dp[j] && (j + step  >= i);
+                if (dp[i]) break;
+                j--;
+            }
+        }
+        return dp[nums.length - 1];
+    }
+
+    // 55-贪心。
+    public boolean canJump_2(int[] nums) {
+        // 使用贪心算法，实时保存**能够跳跃的最远距离**。
+        if (nums.length == 1) return true;
+        int max = nums[0];
+        // 关键就是保存能跳跃的最远距离。
+        for (int i = 1; i < nums.length; i++) {
+            if (max >= i) {
+                max = Math.max(max, i + nums[i]);
+                if (max >= nums.length - 1) return true;
+            }
+        }
+        return false;
+    }
+
+    // 45
+    public int jump(int[] nums) {
+        // dp，题意会保证肯定可以跳完。
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            dp[i] = Integer.MAX_VALUE;
+        dp[0] = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (j + nums[j] >= i)
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+            }
+        }
+        return dp[nums.length - 1];
+    }
+
+    public int jump_2(int[] nums) {
+        // 贪心-维护最远能到的地方。
+        int res = 0;
+        int max = 0, right = 0;
+        // right 表示右边界，max 表示边界内的 i 最远能去哪里。
+        for (int i = 0; i < nums.length - 1; i++) {
+            // 最后一步 nums.length 不算（已经到了 nums 的末尾），因此要 -1。
+            max = Math.max(max, i + nums[i]);
+            if (i == right) {
+                res++;
+                right = max;
+                // 更新边界。
+            }
+        }
+        return res;
+    }
+
+    // 763
+    public List<Integer> partitionLabels(String s) {
+        // 需要有一个 map list 记录每种字符最靠后的 index。
+        List<Integer> res = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), i);
+        }
+        int last = 0;
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            last = Math.max(last, map.get(s.charAt(i)));
+            length++;
+            if (i == last) {
+                res.add(length);
+                length = 0;
+            }
+        }
+        return res;
+    }
+
+
+
 
 }
---
+++ Java/summer/_81_90.java
@@ -0,0 +1,110 @@
+package Java.summer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class _81_90 {
+    // 70-32
+    // 70
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    // 118
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 1) return List.of(List.of(1));
+        if (numRows == 2) return List.of(List.of(1), List.of(1, 1));
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(List.of(1));
+        list.add(List.of(1, 1));
+        for (int i = 2; i < numRows; i++) {
+            List<Integer> inner = new ArrayList<>();
+            inner.add(1);
+            for (int j = 1; j < i; j++) {
+                inner.add(list.get(i - 1).get(j - 1) + list.get(i - 1).get(j));
+            }
+            inner.add(1);
+            list.add(inner);
+        }
+        return list;
+    }
+
+    // 198
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+        return dp[nums.length - 1];
+    }
+
+    // 279
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        // dp[0] 不考虑。
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] =
+            }
+
+        }
+
+        return -1;
+    }
+}
+
+// 饿了么。
+class Solution {
+    public static int minRefuelStops(int target, int startFuel, int[][] stations) {
+        // 先去判断能不能到，贪心!
+        long cur = 0;
+        long fuel = startFuel;
+        for (int i = 0; i < stations.length; i++) {
+            if (stations[i][0] - cur <= fuel) {
+                fuel -= (stations[i][0] - cur);
+                fuel += stations[i][1];
+                cur = stations[i][0];
+            } else return -1;
+        }
+        if (fuel < target - cur) return -1;
+
+        cur = 0;
+        fuel = startFuel;
+        int res = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        // 利用最大堆存储所有的油。
+        for (int i = 0; i < stations.length; ) {
+            if (stations[i][0] - cur <= fuel) {
+                // 能加到油。
+                queue.add(stations[i][1]);
+                fuel -= (stations[i][0] - cur);
+                cur = stations[i][0];
+                i++;
+            } else {
+                int add = queue.poll();
+                fuel += add;
+                res++;
+            }
+        }
+        while (fuel < target - cur) {
+            // 要想走完全程，还得加油。
+            int add = queue.poll();
+            fuel += add;
+            res++;
+        }
+        return res;
+    }
+
+}
